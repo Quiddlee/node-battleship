c4b8227e35eda3cbfce6a13a199fb2668ed19eae
Bohdan <bogdanscherbinadev@gmail.com>
2024-02-16 17:48:43 +0200

    refactor: add create game controller JSDoc

--- src/controllers/gamesController.ts
+++ src/controllers/gamesController.ts
@@ -3,6 +3,13 @@ import { CreateGameDataRes } from '../models/game/types/types';
 import { MsgType } from '../types/enums';
 import { Cb } from '../types/types';
 
+/**
+ * Creates a new game with the given user as the host and sends the game data to all clients.
+ * @param {Object} clients - An object containing the clients to send the data to.
+ * @param {Cb<MsgType.ADD_USER_ROOM>} clients.sendEach - A callback function that sends a message to each client.
+ * @param {Object} ws - The WebSocket connection of the user.
+ * @param {number} ws.id - The id of the user.
+ */
 export const createGame: Cb<MsgType.ADD_USER_ROOM> = ({ clients, ws }) => {
   const { gameId, playerId } = gamesDB.createGame(ws.id);
 
