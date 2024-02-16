5159966f56480b667eeeffffc7d940461d880fe0
Bohdan <bogdanscherbinadev@gmail.com>
2024-02-16 16:53:56 +0200

    refactor: remove user uuid field

--- src/models/user/user.ts
+++ src/models/user/user.ts
@@ -1,18 +1,13 @@
-import { randomUUID } from 'node:crypto';
-
 export class User {
   readonly #login: string;
 
   readonly #password: string;
-
-  readonly #id: string;
 
   #wins: number = 0;
 
   constructor(login: string, password: string) {
     this.#login = login;
     this.#password = password;
-    this.#id = randomUUID();
   }
 
   public get login() {
@@ -21,10 +16,6 @@ export class User {
 
   public get password() {
     return this.#password;
-  }
-
-  public get id() {
-    return this.#id;
   }
 
   public get wins() {
