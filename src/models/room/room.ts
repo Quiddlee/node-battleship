4fca59d128d556826cc3211e94dbca430694040a
Bohdan <bogdanscherbinadev@gmail.com>
2024-02-17 15:24:55 +0200

    refactor: rename class fields

--- src/models/room/room.ts
+++ src/models/room/room.ts
@@ -1,22 +1,22 @@
 export class Room {
   readonly roomId: number;
 
-  readonly roomPlayersId: [number] | [number, number];
+  readonly roomPlayerIds: [number] | [number, number];
 
   constructor(roomId: number, creatorId: number) {
     this.roomId = roomId;
-    this.roomPlayersId = [creatorId];
+    this.roomPlayerIds = [creatorId];
   }
 
   public addUser(userIndex: number) {
     // TODO: decide what to do if the user already in the room
-    const isUserAlreadyInTheRoom = this.roomPlayersId.includes(userIndex);
+    const isUserAlreadyInTheRoom = this.roomPlayerIds.includes(userIndex);
 
     if (isUserAlreadyInTheRoom)
       throw new Error(
         "You can't join the room when you are already in the room",
       );
 
-    this.roomPlayersId.push(userIndex);
+    this.roomPlayerIds.push(userIndex);
   }
 }
